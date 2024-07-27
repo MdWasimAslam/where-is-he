d25c4d27a586e992b831f1d7f5a4f49d556dfafb
Md Wasim Aslam <wasimaslam2897@gmail.com>
2024-07-27 17:45:41 +0530

    MOBILE VALIDATION

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 import { AuthProvider } from './components/AuthContext';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
@@ -7,14 +8,29 @@ import ProtectedRoute from './components/ProtectedRoute';
 import TrackingUI from './components/TrackingUI';
 
 const App = () => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   return (
     <AuthProvider>
       <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<ProtectedRoute element={TrackingUI} />} />
-          <Route path="/" element={<Login />} /> {/* Optional default route */}
-        </Routes>
+        {isMobile ? (
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<ProtectedRoute element={TrackingUI} />} />
+            <Route path="/" element={<Login />} /> {/* Optional default route */}
+          </Routes>
+        ) : (
+          <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            fontSize: '24px',
+            
+          }}
+          >Only available for mobile devices</div>
+        )}
       </Router>
     </AuthProvider>
   );
